e977e4310056d23337caafba684d409e84d51be2
Xiaoxing Hu <xiaoxing@huxx.org>
2019-11-16 21:48:07 +1300

    fix path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: `gatsby-starter-blorg`,
   siteMetadata: {
     title: `Gatsby With Orga`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
